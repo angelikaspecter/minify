ef969add9ffed8f474c2cc1a0613e65db314fe02
Angelika Specter <angelikaspecter@gmail.com>
2024-08-04 05:55:03 +0300

    UPD nav style - add bg color & section style

--- templates/promo.php
+++ templates/promo.php
@@ -1,3 +1,10 @@
 <section class="section section--promo promo">
+    <ul class="promo__list">
+        <li class="promo__item">
 
+        </li>
+        <li class="promo__item">
+
+        </li>
+    </ul>
 </section>
