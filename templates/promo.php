4f353e95f52dceccbbc61e1277851f1ba52795b7
Angelika Specter <angelikaspecter@gmail.com>
2024-08-04 06:03:00 +0300

    UPD section style promo

--- templates/promo.php
+++ templates/promo.php
@@ -4,7 +4,9 @@
 
         </li>
         <li class="promo__item">
-
+            <div class="promo__img">
+                <img src="img/img_1.webp" width="553" height="575" alt="Ariene Ray" class="img">
+            </div>
         </li>
     </ul>
 </section>
