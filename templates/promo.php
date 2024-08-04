cbdf43b56b13c90ae0264e33950a06239f9d8913
Angelika Specter <angelikaspecter@gmail.com>
2024-08-05 01:14:30 +0300

    Promo Section Complete

--- templates/promo.php
+++ templates/promo.php
@@ -1,7 +1,47 @@
 <section class="section section--promo promo">
     <ul class="promo__list">
         <li class="promo__item">
-
+            <header class="promo__header">
+                <p class="desc desc--extralarge desc--medium desc--middle">Hey!</p>
+                <h1 class="title title--primary title--semibold title--dark">I’m <span class="title--color">Ariene
+                        Rey</span>. <br>
+                    an <span class="title--elem">UI/UX Designer</span>.</h1>
+            </header>
+            <div class="promo__content">
+                <p class="desc desc--large desc--regular desc--dark">UX Designer based in Jakarta, Indonesia.
+                    I am designing with a minimal and beautiful design in mind.</p>
+                <ul class="social">
+                    <li class="social__item">
+                        <p class="desc desc--extrasmall desc--regular desc--middle">Follow Me</p>
+                    </li>
+                    <li class="social__item">
+                        <a href="#" class="social__link">
+                            <img src="img/instagram.svg" width="24" height="24" alt="instagram"
+                                class="icon icon--normal">
+                        </a>
+                    </li>
+                    <li class="social__item">
+                        <a href="#" class="social__link">
+                            <img src="img/youtube.svg" width="24" height="24" alt="youtube" class="icon icon--normal">
+                        </a>
+                    </li>
+                    <li class="social__item">
+                        <a href="#" class="social__link">
+                            <img src="img/dribbble.svg" width="24" height="24" alt="dribble" class="icon icon--normal">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <footer class="promo__footer">
+                <a href="mailto:" class="btn btn--primary">
+                    <img src="img/mail.svg" width="24" height="24" alt="mail icon" class="icon icon--normal">
+                    <span class="desc desc--normal desc--bold desc--light">Mail Me</span>
+                </a>
+                <a class="btn btn--secondary" download>
+                    <img src="img/download.svg" width="24" height="24" alt="download icon" class="icon icon--normal">
+                    <span class="desc desc--normal desc--bold desc--dark">Download CV</span>
+                </a>
+            </footer>
         </li>
         <li class="promo__item">
             <div class="promo__img">
